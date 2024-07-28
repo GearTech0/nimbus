bd59e30d8ba885732efb3bc4a1c3f7db22d8a35f
theonewhostands <cdaquaris@gmail.com>
2024-07-28 11:11:58 -0400

    feat: add create raindrop functionality and basic types

--- cmd/nimbus/main.go
+++ cmd/nimbus/main.go
@@ -8,12 +8,66 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type NimbusClient struct {
 	baseurl string
 	bearer  string
 	handle  *http.Client
+}
+
+type CollectionType struct {
+	Id string `json:"$id,omitempty"`
+}
+
+type HighlightType struct {
+	Id      string   `json:"_id"`
+	Text    string   `json:"text"`
+	Title   string   `json:"title"`
+	Color   string   `json:"color"`
+	Note    string   `json:"note"`
+	Created string   `json:"created"`
+	Tags    []string `json:"tags"`
+	Link    string   `json:"link"`
+}
+
+type ReminderType struct {
+	Date string `json:"date"`
+}
+
+type RaindropType struct {
+	Created    string         `json:"created,omitempty"`
+	LastUpdate string         `json:"lastUpdate,omitempty"`
+	Order      int64          `json:"order,omitempty,string"`
+	Important  bool           `json:"important,omitempty,string"`
+	Tags       []string       `json:"tags,omitempty"`
+	Media      []string       `json:"media,omitempty"`
+	Cover      string         `json:"cover,omitempty"`
+	Collection CollectionType `json:"collection,omitempty"`
+	Type       string         `json:"type,omitempty"`
+	Excerpt    string         `json:"excerpt,omitempty"`
+	Title      string         `json:"title,omitempty"`
+	Link       string         `json:"link"`
+	Highlights []string       `json:"highlights,omitempty"`
+	Reminder   ReminderType   `json:"reminder,omitempty"`
+}
+
+func (r *RaindropType) ConvertToURLVals() (url.Values, error) {
+	ret := url.Values{}
+
+	ret.Set("link", r.Link)
+	ret.Set("created", r.Created)
+	ret.Set("lastUpdate", r.LastUpdate)
+	ret.Set("order", string(r.Order))
+	ret.Set("important", strconv.FormatBool(r.Important))
+	ret.Set("cover", r.Cover)
+	ret.Set("type", r.Type)
+	ret.Set("excerpt", r.Excerpt)
+	ret.Set("title", r.Title)
+
+	return ret, nil
 }
 
 func (n *NimbusClient) GetCollections() (res *http.Response, e error) {
@@ -54,21 +108,20 @@ func (n *NimbusClient) GetRaindropById(id string) (res *http.Response, e error) 
 	return n.handle.Do(req)
 }
 
-func (n *NimbusClient) CreateRaindrop(v url.Values) (res *http.Response, e error) {
+func (n *NimbusClient) CreateRaindrop(r RaindropType) (res *http.Response, e error) {
 	route := "raindrop/"
 
-	req, err := http.NewRequest("POST", n.baseurl+route, nil)
+	parsedVals, _ := json.Marshal(r)
+	fmt.Println("urlVals: ", string(parsedVals))
+
+	req, err := http.NewRequest("POST", n.baseurl+route, strings.NewReader(string(parsedVals)))
 	if err != nil {
 		return nil, err
 	}
+	req.Header.Set("Authorization", n.bearer)
+	req.Header.Set("Content-Type", "application/json")
 
-	req.Header.Add("Authorization", n.bearer)
-
-	// example, should be replaced by parameters
-	vals := url.Values{}
-	vals.Set("link", "")
-	//req.PostForm.Add()
-	return nil, nil
+	return n.handle.Do(req)
 }
 
 type InvokeResponse struct {
@@ -99,7 +152,11 @@ func run(w http.ResponseWriter, r *http.Request) {
 
 	c := &NimbusClient{bearer: bearer, handle: &http.Client{}, baseurl: url}
 
-	resp, err := c.GetRaindropById("823418374")
+	resp, err := c.CreateRaindrop(RaindropType{
+		Link:       "https://stackoverflow.com/questions/24455147/how-do-i-send-a-json-string-in-a-post-request-in-go",
+		Title:      "How do I fix this stupid bugs...",
+		Collection: CollectionType{"46406303"},
+	})
 	if err != nil {
 		fmt.Println("error: ", err)
 	}
